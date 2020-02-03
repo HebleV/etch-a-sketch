81a419b3842e003a5f5485bfb2d24e960cc53fbd
vheble <visheble@yahoo.com>
2020-02-04 00:58:19 +0530

    Completed etch a sketch

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,74 @@
 
-
+// Select the elements on page
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
+const MOVE_AMOUNT = 50;
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
 // setup canvas for drawing
 
+const {width, height} = canvas;
+
+// create random x and y starting points on canvas
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = MOVE_AMOUNT;
+
+ctx.beginPath();
+ctx.moveTo(x,y);
+ctx.lineTo(x,y);
+ctx.stroke();
+
+// write a draw function
+function draw({ key }){
+  hue = hue + 1;
+  ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
+ console.log(key);
+ ctx.beginPath();
+  ctx.moveTo(x,y);
+  x = x - MOVE_AMOUNT;
+  y = y - MOVE_AMOUNT;
+  switch (key) {
+    case 'ArrowUp': 
+      y = y - MOVE_AMOUNT;
+      break;
+    case 'ArrowDown': 
+      y = y + MOVE_AMOUNT;
+      break;
+    case 'ArrowLeft': 
+      x = x - MOVE_AMOUNT;
+      break;
+    case 'ArrowRight': 
+      x = x + MOVE_AMOUNT;
+      break;
+    default: 
+      break;
+  }
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
+// write a handler function
+function handleKey(e) {
+  if(e.key.includes('Arrow')) {
+    e.preventDefault();
+    draw({ key : e.key }); 
+  }
+}
+
+// clear/shake function
+function clearCanvas(){
+  canvas.classList.add('shake');
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener('animationend', function(){
+    canvas.classList.remove('shake');
+  },{ once:true });
+}
+//listen for arrow keys
+window.addEventListener('keydown', handleKey);
+shakebutton.addEventListener('click', clearCanvas);
